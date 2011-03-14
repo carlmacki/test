ca340c428ac01a120f7bd62617c64a299288312c
Ellen Spertus <ellen.spertus@gmail.com>
2011-03-14 11:37:26 -0700

    I created this application to print the number of members and their names.

---
+++ src/edu/mills/cs180/test/HelloGithub.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package edu.mills.cs180.test;
+
+/**
+ * Simple application to print the number of members and their names.  This
+ * exists to give us practice with github.
+ * 
+ * @author espertus
+ */
+public class HelloGithub {
+	static final int NUM_MEMBERS = 1;
+
+	/**
+	 * Prints the number of members and their names.
+	 * 
+	 * @param args ignored
+	 */
+	public static void main(String[] args) {
+		System.out.println("There are " + NUM_MEMBERS + " members.  Their usernames are:");
+		System.out.println("espertus");
+		// Add your name and increment NUM_MEMBERS.
+		// Also add an @author line, making sure to use your github username.
+	}
+}
