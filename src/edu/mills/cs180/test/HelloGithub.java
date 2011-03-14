e753921366b825fec46a874a95e442bf36dd3ab0
Anna <Anna@10.1.9.198>
2011-03-14 13:40:49 -0700

    added my github username

--- src/edu/mills/cs180/test/HelloGithub.java
+++ src/edu/mills/cs180/test/HelloGithub.java
@@ -8,9 +8,10 @@ package edu.mills.cs180.test;
  * exists to give us practice with github.
  * 
  * @author espertus
+ * @author aforn
  */
 public class HelloGithub {
-	static final int NUM_MEMBERS = 1;
+	static final int NUM_MEMBERS = 2;
 
 	/**
 	 * Prints the number of members and their names.
@@ -20,6 +21,7 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("There are " + NUM_MEMBERS + " members.  Their usernames are:");
 		System.out.println("espertus");
+		System.out.println("aforn");
 		// Add your name and increment NUM_MEMBERS.
 		// Also add an @author line, making sure to use your github username.
 	}
