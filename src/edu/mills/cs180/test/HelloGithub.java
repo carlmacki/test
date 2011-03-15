21123a4b6d5e382eab83574524ba04ecfb96e76c
nwashington <nicole.adero@gmail.com>
2011-03-15 16:42:02 -0700

    I have added my name to the java class HelloGitHub. I have created myself, nicoleader0, as the author and incremented the NUM_MEMBERS to 4.

--- src/edu/mills/cs180/test/HelloGithub.java
+++ src/edu/mills/cs180/test/HelloGithub.java
@@ -7,21 +7,23 @@ package edu.mills.cs180.test;
  * @author espertus
  * @author aforn
  * @author kfeeney
+ * @author nicoleader0
  */
 public class HelloGithub {
-	static final int NUM_MEMBERS = 3; //increment this number when you add yourself
-
+	static final int NUM_MEMBERS = 4; //increment this number when you add yourself
+	
 	/**
 	 * Prints the number of members and their names.
 	 * 
 	 * @param args ignored
 	 */
 	public static void main(String[] args) {
-		// Add your name above and increment NUM_MEMBERS by changing the constant.
+		// Add your name below and increment NUM_MEMBERS by changing the constant.
 		// Also add an @author line above, making sure to use your github username.
 		System.out.println("There are " + NUM_MEMBERS + " members.  Their usernames are:");
 		System.out.println("espertus");
 		System.out.println("aforn");
 		System.out.println("kfeeney");
+		System.out.println("nicoleader0");
 	}
 }
